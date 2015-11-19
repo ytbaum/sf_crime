0daa657a355518f19e099deb59d4311dc83c2375
Yoni Teitelbaum <ytbaum@gmail.com>
2015-11-19 16:54:24 +0200

    this is all that really needs to be in the init file

--- init.R
+++ init.R
@@ -1,11 +1,2 @@
 train <- read.csv("data/train.csv")
-
-
-
-sample.submission <- read.csv("data/sampleSubmission.csv")
-sample.submission.head <- head(sample.submission)
 test <- read.csv("data/test.csv")
-test.head <- head(test)
-
-offenses <- colnames(sample.submission.head)
-offenses <- offenses[!(offenses == "Id")] # remove "Id" from this list, since it's not an offense
